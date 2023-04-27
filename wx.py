2e25553bca11d97168281683b3b6b8377928bdd2
mmagnus <mag_dex@o2.pl>
2023-04-27 13:23:21 -0400

    run wx.py for multiple --stop-arf

---
+++ wx.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import print_function
+import argparse
+from icecream import ic
+import sys
+ic.configureOutput(outputFunction=lambda *a: print(*a, file=sys.stderr))
+ic.configureOutput(prefix='> ')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    #parser.add_argument('-', "--", help="", default="")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="be verbose")
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    for i in range(1, 9):
+        cmd = 'python rf.py --stop-arf ' + str(i) + ' -v ./examples/4GXY_min.pdb -o m' + str(i) + '.pdb'
+        ic(cmd)
+        import os
+        os.system(cmd)
