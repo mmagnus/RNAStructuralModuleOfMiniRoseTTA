af7d3f1a5abd8d4d3e4aaf1c409eda18b7ff3b24
mmagnus <mag_dex@o2.pl>
2023-05-03 10:19:21 -0400

    better (slightly) test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-python rf.py ./examples/1xjr.pdb
-#python rf.py ./examples/4GXY_min.pdb #1xjr.pdb
+#python rf.py ./examples/1xjr.pdb
+python rf.py ./examples/4GXY_min.pdb -o examples/4GXY_min_output.pdb #1xjr.pdb
 #LHFNpQ
