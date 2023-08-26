1ab41e7d36cbbdb08818a75df80aea819598bbcf
mmagnus <mag_dex@o2.pl>
2023-08-26 15:55:56 -0400

    up test

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
-#python rf.py ./examples/1xjr.pdb -v
-python rf.py ./examples/4GXY_min.pdb -o examples/4GXY_min_output.pdb -v #1xjr.pdb
+python rf.py ./examples/4GXY_min.pdb -v -o 4GXY_min_at0_chemicals.pdb # 4GXY_min_at0.pdb
+#python rf.py ./examples/1xjr.pdb -v -o 1xjr_chemicals.pdb
+#python rf.py --chemicals chemicals_fix.py ./examples/1xjr.pdb -v -o 1xjr_chemicals_fix.pdb
+
+# --chemicals CHEMICALS 
+#rm examples/output.pdb
+#
+#python rf.py --rtf0 --torsion0 --stop-artf 1 ./examples/4GXY_min.pdb -o examples/4GXY_min_out_t0_rtf0_artf1.pdb -v
+#python rf.py --rtf0 --torsion0 --stop-artf 2 ./examples/4GXY_min.pdb -o outputs/t0s2.pdb #4GXY_min_out_tX_rtf0_artf2.pdb -v
+#python rf.py.py --rtf0            --stop-artf 2 ./examples/4GXY_min.pdb -o outputs/tXs2.pdb -v
 #python ./translate.py ./examples/4GXY_min.pdb
 #python ./translate.py ./examples/4GXY_min_rt.pdb
 #LHFNpQ
+#open output.pdb
